7ca8b774c99f0f5a8f1f48b69092563860456b63
Q <greenwindstorm333@yahoo.com>
2011-06-06 19:53:45 -0400

    finished the graphics section a bit

--- WallStreet/src/WallStreet/WallStreet.java
+++ WallStreet/src/WallStreet/WallStreet.java
@@ -11,11 +11,16 @@ public class WallStreet {
     Human human;
 
     public WallStreet() {
+        drawGraphics();
+    }
+
+    private void drawGraphics(){
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        human = new Human();
-        frame.getContentPane().add(human);
         frame.setSize(500, 500);
+        human = new Human(10);
+
+        frame.getContentPane().add(human);
         frame.setVisible(true);
     }
 
@@ -28,12 +33,24 @@ public class WallStreet {
 
     public class Human extends JPanel {
 
+        int _numHumans;
+
+        public Human(int numHumans){
+            super();
+            _numHumans = numHumans;
+        }
+
         public void paintComponent(Graphics g) {
+            Image[] images = new Image[_numHumans];
             Image img = new ImageIcon("C:\\Users\\English\\Documents\\NetBeansProjects\\CS-project\\WallStreet\\src\\WallStreet\\Human.jpg").getImage();
-            Image img2 = img.getScaledInstance(100, 100, 2);
-            img = img2;
-            g.drawImage(img, 0, 0, this);
-            g.drawImage(img2, 0, 100, this);
+            img = img.getScaledInstance(frame.getWidth()/_numHumans, frame.getHeight()/_numHumans, 2);
+            for(int i = 0; i < images.length; i++){
+                images[i] = img;
+                g.drawImage(images[i], i * frame.getWidth() / _numHumans , 0, this);
+            }
+            Image img2 = new ImageIcon("C:\\Users\\English\\Documents\\NetBeansProjects\\CS-project\\WallStreet\\src\\WallStreet\\WallStreet.jpg").getImage();
+            img2 = img2.getScaledInstance(frame.getWidth(), (int)(frame.getHeight() * (_numHumans - 1.0)/(_numHumans)), 1);
+            g.drawImage(img2, 0, frame.getHeight()/_numHumans, this);
         }
     }
 
