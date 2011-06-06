fe63d2057b3301346e578fd06f814c625a21bdb7
Q <greenwindstorm333@yahoo.com>
2011-06-06 10:03:47 -0400

    the images finally work!!!!!!!!!!!!!!!!!!

--- WallStreet/src/WallStreet/Human.java
+++ WallStreet/src/WallStreet/Human.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Human extends JPanel {
 
     public void paintComponent(Graphics g) {
-        Image img = new ImageIcon("Human.jpg").getImage();
+        Image img = new ImageIcon("C:\\Users\\English\\Documents\\NetBeansProjects\\CS-project\\WallStreet\\src\\WallStreet\\Human.jpg").getImage();
         g.drawImage(img, 3, 4, this);
     }
 }
