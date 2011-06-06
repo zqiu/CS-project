ed296fce732bfaa07af76425d411decdf0552bc8
Q <greenwindstorm333@yahoo.com>
2011-06-06 18:39:40 -0400

    got rid of Human.java and put it in WallStreet.java

--- WallStreet/src/WallStreet/WallStreet.java
+++ WallStreet/src/WallStreet/WallStreet.java
@@ -1,6 +1,7 @@
 package WallStreet;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WallStreet {
 
@@ -24,6 +25,14 @@ public class WallStreet {
 
     public String toString() {
         return "complete this";
+    }
+
+    public class Human extends JPanel {
+
+        public void paintComponent(Graphics g) {
+            Image img = new ImageIcon("C:\\Users\\English\\Documents\\NetBeansProjects\\CS-project\\WallStreet\\src\\WallStreet\\Human.jpg").getImage();
+            g.drawImage(img, 3, 4, this);
+        }
     }
 
     public static void main(String[] arg) {
